ad1bf8d4ae81f0307be41ff25a8f03ad2aee5621
Marcin Swornowski <swor71@gmail.com>
2018-09-29 14:40:38 +0200

    Add displaying of ten random jokes

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,11 +1,29 @@
 <template>
-  <div id="app">hi
-    {{$store.state}}
+  <div id="app">
+    <h3>Jokester</h3>
+    <button class="btn btn-danger" @click="initJokes">Get Jokes</button>
+    <br>
+    <br>
+    <div class="row">
+      <Joke 
+        v-for="(joke, index) in $store.state.jokes"
+        :joke="joke"
+        :key="index"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import { mapActions } from 'vuex';
+import Joke from './Joke';
+
+export default {
+  methods: mapActions(['initJokes']),
+  components: {
+    Joke
+  }
+};
 </script>
 
 <style>
